220cd0f3892b6713dcbea4e5311ef93f4de2705c
joao pedro vr <joao.rodrigues@pisomtech.com.br>
2023-05-08 14:56:24 -0300

    data update

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,50 @@
 export const dataFake = [
   {
-     "id":"1",
-     "title":"Novo Home de Ferro em 3D",
-     "description":"Marvel Anuncia novo homem de ferro",
-     "photoCover": "https://rare-gallery.com/mocahbig/84844-iron-man-superheroes-hd-4k-5k-artist-artwork.jpg"
+    "id":"1",
+    "title":"Praia Clube decide título da Superliga contra o Minas neste domingo (7)",
+    "description":"O Praia Clube, enfim, bateu o Minas em uma decisão de Superliga feminina de vôlei e sagrou-se campeão da edição 2022/2023. Foi neste domingo (7), no Ginásio Sabiazinho, em Uberlândia (MG), e o triunfo veio de forma completa, por 3 sets a 0 (parciais de 25 x 22, 25 x 22 e 26 x 24).",
+    "photoCover": "https://diariodeuberlandia.com.br/images/noticias/33546/06055442_img_0070.jpg",
   },
   {
     "id":"2",
-    "title":"Nova Série anunciada no Disney Plus",
-    "description":"ola BLABLABLABLA",
-    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
- }
+    "title":"Representantes de associações parceiras da Futel são convocados pela seleção de bocha paralímpica",
+    "description":"O paratleta Mateus Carvalho e o assistente esportivo Oscar Carvalho, ambos da equipe CDDU/Praia Clube/Futel, e o treinador Glênio Fernandes, da Aparu/Futel/Praia Clube, foram convocados para a fase de treinamento da seleção brasileira de bocha paralímpica. O evento acontecerá em São Paulo, entre os dias 15 e 21 de maio, como preparação para competições nacionais e internacionais previstas para este ano.As convocações de Mateus, Oscar e Glênio para a seleção brasileira têm sido frequentes em treinamentos e competições, o que tem ligação direta com o trabalho desenvolvido pela Futel.",
+    "photoCover": "https://www.uberlandia.mg.gov.br/wp-content/uploads/2023/05/Bocha-Mateus-Oscar-e-Glenio.jpg",
+  },
+  {
+    "id":"3",
+    "title":"Cineteatro Nininha Rocha recebe o espetáculo “Melhores Irmãos” neste fim de semana",
+    "description":"O Cineteatro Nininha Rocha, em Uberlândia, recebe neste fim de semana o espetáculo “Melhores Irmãos”, apresentado pela Cia. Líquida de Teatro. A peça é uma comédia agridoce inédita no Brasil que mostra como os laços afetivos podem ser retomados e transformados nos momentos mais inesperados.O espetáculo é estrelado pelos atores Getúlio Góis e Samuel Giacomelli, com direção de Rafael Michalichem. Os personagens da história vivem irmãos e enfrentam velhas brigas enquanto revivem as memórias de sua mãe. “A família Best, retratada na peça, tem suas peculiaridades, mas representa bem a diversidade singular que existe em muitas famílias. Um irmão mais certinho, outro mais desorganizado, um cãozinho travesso e uma mãe que viveu experiências excêntricas: esses personagens celebram as diferenças que existem em cada grupo familiar. Quem tem mãe, irmão ou cachorro vai gostar!”, destaca o diretor. ",
+    "photoCover": "https://diariodeuberlandia.com.br/images/noticias/33448/19041804_Teatro.jpg",
+  },
+  {
+    "id":"4",
+    "title":"Shows com Gusttavo Lima e Ana Castela, espetáculos teatrais e mais: veja opções para o fim de semana, em Uberlândia",
+    "description":"O fim de semana promete ser agitado em Uberlândia. Neste sábado (15), a cidade recebe um evento com shows dos sertanejos Gusttavo Lima e Ana Castela. Além destas atrações, a agenda cultural do Diário traz como destaque dois espetáculos teatrais e uma programação diversificada de bares, pubs e casas noturnas.",
+    "photoCover": "https://diariodeuberlandia.com.br/images/noticias/33404/13045211_WhatsApp_I.jpeg",
+  },
+  {
+    "id":"5",
+    "title":"Sunset de samba e evento especial de Dia das Mães agitam o fim de semana, em Uberlândia",
+    "description":"O fim de semana, em Uberlândia, será repleto de atrações para todos os gostos e idades. No sábado (6), o público poderá se divertir ao som do projeto 'Coisas da Terra Acústico', com um show da cantora Lúcia Costa. Já no domingo (7), uma sunset de samba promete agitar o dia com diversas atrações musicais. Também estão previstos eventos em comemoração ao Dia das Mães. Confira os destaques na Agenda Diário.",
+    "photoCover": "https://diariodeuberlandia.com.br/images/noticias/33529/04055244_WhatsApp_I.jpeg",
+  },
+  {
+    "id":"6",
+    "title":"Sétima morte por dengue é confirmada, em Uberlândia",
+    "description":"Uberlândia registrou, nesta segunda (8), mais uma morte causada por dengue. Com a atualização, a cidade chegou a sete vítimas da doença apenas em 2023. Ainda de acordo com os dados disponibilizados pelo Painel de Monitoramento de Casos da Secretaria de Estado da Saúde (SES-MG), o número de casos confirmados subiu para 15.189.Segundo a secretaria, o município tem outras seis mortes sob investigação. Em relação à chikungunya, Uberlândia tem 11 casos confirmados, sendo que outros 44 são tratados como prováveis. A cidade não tem nenhum registro de pacientes com zika vírus.",
+    "photoCover": "https://images.weserv.nl/?url=diariodeuberlandia.com.br/images/noticias/33553/08054736_aedes_aegy.png&w=575&h=590&t=square&a=attention&output=webp&q=75&default=ssl:diariodeuberlandia.com.br/images/noticias/33553/08054736_aedes_aegy.png"
+ },
+ {
+   "id":"7",
+   "title":"Concessionária oferta 160 vagas de emprego em Uberlândia e região",
+   "description":"A EPR Triângulo, concessionária que integra o programa de concessão de Minas Gerais, está com vagas abertas para atuação em oito cidades do Triângulo Mineiro, incluindo Uberlândia, Nova Ponte, Perdizes, Monte Carmelo, Patrocínio, Uberaba, Conceição das Alagoas e Iraí de Minas.Ao todo, são mais de 160 oportunidades nas atividades operacionais nas rodovias, para as jornadas diurnas e noturnas, com níveis de escolaridade que vão do ensino médio ao superior e experiência em gestão de pessoas. ",
+   "photoCover": "https://images.weserv.nl/?url=diariodeuberlandia.com.br/images/noticias/33552/08051959_20150627_1.jpg&w=575&h=285&t=square&a=attention&output=webp&q=75&default=ssl:diariodeuberlandia.com.br/images/noticias/33552/08051959_20150627_1.jpg"
+},
+{
+ "id":"8",
+ "title":"Mulher é encontrada morta com sinais de violência em quarto de motel, em Uberlândia",
+ "description":"Uma mulher de 55 anos foi encontrada morta com sinais de estrangulamento dentro de um motel, na manhã do último sábado (6), no bairro Panorama, em Uberlândia. Segundo informações, a vítima estava com a própria roupa enrolada no pescoço. O principal suspeito de cometer o crime é um homem de 37 anos, que ainda não foi encontrado pela Polícia Militar (PM).De acordo com o boletim de ocorrência, o casal deu entrada no motel na noite de sexta-feira (5). O estabelecimento fica localizado na avenida Aldo Borges Leão. No dia seguinte, por volta de 6h30, o autor pagou a conta e disse que a mulher continuaria no quarto até 9h, quando encerraria o tempo contratado.Depois de algum tempo, uma funcionária que trabalha no local bateu na porta do quarto, mas não obteve resposta. Ao entrar no cômodo, ela encontrou a vítima morta em cima da cama. O trabalho de perícia confirmou que o autor usou um macacão da vítima para estrangulá-la.O corpo da mulher foi encaminhado à funerária. A Polícia Militar busca pelo homem responsável pelo feminicídio.",
+ "photoCover": "https://images.weserv.nl/?url=diariodeuberlandia.com.br/images/noticias/33548/08052645_pm_divulga.jpeg&w=317&h=210&t=square&a=attention&output=webp&q=75&default=ssl:diariodeuberlandia.com.br/images/noticias/33548/08052645_pm_divulga.jpeg"
+ },
 ]
