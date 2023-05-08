49bae0c74974088d6424c7c08f628909fba2605d
joao pedro vr <joao.rodrigues@pisomtech.com.br>
2023-05-08 14:08:21 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
+  @Input()
+  photoCover = "";
+  @Input()
+  cardTitle = "";
+  @Input()
+  cardDescription = "";
+  @Input()
+  Id:string = "0";
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+     "id":"1",
+     "title":"Novo Home de Ferro em 3D",
+     "description":"Marvel Anuncia novo homem de ferro",
+     "photoCover": "https://rare-gallery.com/mocahbig/84844-iron-man-superheroes-hd-4k-5k-artist-artwork.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Nova Série anunciada no Disney Plus",
+    "description":"ola BLABLABLABLA",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+ }
+]
